c343b497822a15a40864b590131c1010167be943
qjvk2880 <qjvk2880@gmail.com>
2023-01-03 15:48:43 +0900

    🎉프로젝트 생성

---
+++ src/test/java/com/BoardProject/springboard/SpringboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BoardProject.springboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
